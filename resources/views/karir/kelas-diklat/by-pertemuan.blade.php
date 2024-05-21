9a4d3310cb4b55dc5a5061fb272c31e0e0d95d29
khairul <arifinkhairul46@gmail.com>
2024-05-21 21:17:31 +0700

    add nav-link active

--- resources/views/karir/kelas-diklat/by-pertemuan.blade.php
+++ resources/views/karir/kelas-diklat/by-pertemuan.blade.php
@@ -93,4 +93,13 @@
             </form>
         </div>
     </div>
+
+    <script>
+        document.querySelectorAll(".nav-link").forEach((link) => {
+            if (link.href === window.location.href) {
+                link.classList.add("active");
+                link.setAttribute("aria-current", "page");
+            }
+        });
+    </script>
 @endsection
