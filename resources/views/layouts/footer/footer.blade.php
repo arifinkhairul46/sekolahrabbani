e60865c1bd1f8f0cb8d00ffdc8e5739a8a62fd09
khairul <arifinkhairul46@gmail.com>
2024-07-25 15:56:39 +0700

    add gallery footer

--- resources/views/layouts/footer/footer.blade.php
+++ resources/views/layouts/footer/footer.blade.php
@@ -35,12 +35,41 @@
             </div>
             <div class="col-md text-white mt-3">
                 <h6> Galeri </h6>
-                <a href="#" class="text-white" style="text-decoration: none"> Tentang Sekolah </a> <br>
-                <a href="#" class="text-white" style="text-decoration: none"> Guru Karyawan </a> <br>
-                <a href="#" class="text-white" style="text-decoration: none"> Sarana </a> <br>
-                <a href="#" class="text-white" style="text-decoration: none"> Karir </a> <br>
+                <div class="slideshow-container">
+                    <div class="mySlides fade">
+                        <img src="{{ asset('assets/images/gallery_1.png') }}" style="max-height: 150px">
+                    </div>
+                    <div class="mySlides fade">
+                        <img src="{{ asset('assets/images/gallery_4.png') }}" style="max-height: 150px">
+                    </div>
+                    <div class="mySlides fade">
+                        <img src="{{ asset('assets/images/gallery_3.png') }}" style="max-height: 150px">
+                    </div>
+                </div>
             </div>
         </div>
        
     </div>
 </div>
+
+<script>
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+//   dots[slideIndex-1].classList += " active";
+  setTimeout(showSlides, 3500); // Change image every 5 seconds
+}
+</script>
