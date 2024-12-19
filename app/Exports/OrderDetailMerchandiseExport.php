ac8bd21451c12557db79a6e28a5d7d0b3027064b
khairul <arifinkhairul46@gmail.com>
2024-12-19 10:48:32 +0700

    update export excel ukuran id

--- app/Exports/OrderDetailMerchandiseExport.php
+++ app/Exports/OrderDetailMerchandiseExport.php
@@ -16,7 +16,7 @@ class OrderDetailMerchandiseExport implements FromCollection, WithHeadings
         // return OrderDetailMerchandise::all();
         $data = OrderDetailMerchandise::select('t_pesan_merchandise_detail.no_pesanan', 't_pesan_merchandise_detail.nama_siswa', 't_pesan_merchandise_detail.lokasi_sekolah',
                     't_pesan_merchandise_detail.nama_kelas', 'mm.nama_produk', 'mwk.warna', 'mtd.judul as template',  
-                    'mus.ukuran_seragam', 'mku.kategori', 'tdp.nis', 't_pesan_merchandise_detail.quantity', 't_pesan_merchandise_detail.created_at' )
+                    't_pesan_merchandise_detail.ukuran_id', 'mku.kategori', 'tdp.nis', 't_pesan_merchandise_detail.quantity', 't_pesan_merchandise_detail.created_at' )
                     ->leftJoin('t_pesan_merchandise as tpm', 'tpm.no_pesanan', 't_pesan_merchandise_detail.no_pesanan')
                     ->leftJoin('m_merchandise as mm', 'mm.id', 't_pesan_merchandise_detail.merchandise_id')
                     ->leftJoin('m_warna_kaos as mwk', 'mwk.id', 't_pesan_merchandise_detail.warna_id')
